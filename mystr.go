7a4d9ad7d2c1b7d2a1679444f907998c980688b8
Sundara <C-Sundara.Senthil1@charter.com>
2019-09-29 19:20:47 -0400

    Add validation

--- mystr.go
+++ mystr.go
@@ -1,14 +1,16 @@
 package mystr
 
-
 func StrMirror(inStr string) bool {
 	inStrLen := len(inStr)
-	j := inStrLen-1
-	for i:=0;  i<j; i++ {
+	if inStrLen < 1 {
+		return false
+	}
+	j := inStrLen - 1
+	for i := 0; i < j; i++ {
 		if inStr[i] != inStr[j] {
 			return false
 		}
 		j = j - 1
-	} 
+	}
 	return true
 }
